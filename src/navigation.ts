c619072da5cc3db5e0738b0f8435cbeb37044b29
stahnkiong <stahn85@gmail.com>
2025-02-05 20:04:20 +0800

    solutions retails

--- src/navigation.ts
+++ src/navigation.ts
@@ -60,7 +60,7 @@ export const headerData = {
           href: getPermalink('/logistics'),
         },
         {
-          text: 'Retail',
+          text: 'Retail & Ecommerce',
           href: getPermalink('/retail'),
         },
         {
