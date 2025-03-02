d86a765266646fb4a39480b65962c3a074978a7b
stahnkiong <stahn85@gmail.com>
2025-03-02 19:33:55 +0800

    contact us

--- src/navigation.ts
+++ src/navigation.ts
@@ -4,24 +4,7 @@ export const headerData = {
   links: [
     {
       text: 'Profile',
-      links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
-      ],
+      href: getPermalink('/about'),
     },
     {
       text: 'Services',
